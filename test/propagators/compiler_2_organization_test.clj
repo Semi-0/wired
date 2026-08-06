ce606e7b8e28196f2cbf545daa70b28182facdba
PRim <longpp17@163.com>
2026-08-07 01:20:55 +0200

    Canonicalize Compiler 2 surface rewrites

--- test/propagators/compiler_2_organization_test.clj
+++ test/propagators/compiler_2_organization_test.clj
@@ -4,8 +4,8 @@
             [propagators.compiler-2.compiler.declarations :as declarations]
             [propagators.compiler-2.compiler.handlers :as handlers]
             [propagators.compiler-2.compiler.predicates :as predicates]
-            [propagators.compiler-2.core :as core]
-            [propagators.compiler-2.compiler.core :as compiler-shim]
+            [propagators.compiler-2.deprecated.legacy-core :as legacy-core]
+            [propagators.compiler-2.deprecated.compiler-core :as compiler-shim]
             [propagators.compiler-2.deprecated.core :as deprecated-core]
             [propagators.compiler-2.deprecated.synchronous :as synchronous]
             [propagators.compiler-2.main :as main]
@@ -25,9 +25,10 @@
          (-> #'predicates/literal? meta :ns ns-name))))
 
 (deftest deprecated-compilers-remain-source-compatible
-  (is (:deprecated (meta (find-ns 'propagators.compiler-2.core))))
+  (is (:deprecated
+       (meta (find-ns 'propagators.compiler-2.deprecated.legacy-core))))
   (is (:deprecated (meta (find-ns
-                          'propagators.compiler-2.compiler.core))))
+                          'propagators.compiler-2.deprecated.compiler-core))))
   (is (:deprecated (meta #'compiler-shim/default-compiler)))
   (is (:deprecated (meta (find-ns
                           'propagators.compiler-2.deprecated.core))))
@@ -37,16 +38,19 @@
                           'propagators.compiler-2.predicate-core))))
   (is (:deprecated (meta (find-ns
                           'propagators.compiler-2.deprecated.synchronous))))
-  (is (:deprecated (meta #'core/default-compiler)))
+  (is (:deprecated (meta #'legacy-core/default-compiler)))
   (is (:deprecated (meta #'predicate-shim/default-compiler)))
-  (is (identical? predicate-shim/default-compiler core/default-compiler))
-  (is (identical? core/default-compiler deprecated-core/default-compiler))
-  (is (identical? core/compile-literal synchronous/compile-literal))
-  (is (identical? core/declare-closure declarations/declare-closure))
+  (is (identical? predicate-shim/default-compiler
+                  legacy-core/default-compiler))
+  (is (identical? legacy-core/default-compiler
+                  deprecated-core/default-compiler))
+  (is (identical? legacy-core/compile-literal synchronous/compile-literal))
+  (is (identical? legacy-core/declare-closure
+                  declarations/declare-closure))
   (is (instance? clojure.lang.MultiFn main/g:compile))
-  (is (identical? main/g:compile core/g:compile))
-  (is (identical? main/g:apply core/g:apply))
-  (is (identical? main/g:advance core/g:advance)))
+  (is (identical? main/g:compile legacy-core/g:compile))
+  (is (identical? main/g:apply legacy-core/g:apply))
+  (is (identical? main/g:advance legacy-core/g:advance)))
 
 (deftest live-runtime-is-owned-by-propagators
   (is (not (:deprecated (meta (find-ns
